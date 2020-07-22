241739b96de5702677451fe1d4fda4aaa8a1b225
e.alihangt@gmail.com <e.alihangt@gmail.com>
2020-07-23 01:31:58 +0600

    Добавьте файлы проекта.

---
+++ asptodo/Data/asptodoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using asptodo.Models;
+
+namespace asptodo.Data
+{
+    public class asptodoContext : DbContext
+    {
+        public asptodoContext (DbContextOptions<asptodoContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<asptodo.Models.Note> Note { get; set; }
+    }
+}
---
+++ asptodo/Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asptodo.Models
+{
+    public class Note
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Display(Name = "Дата создания")]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        [Display(Name = "Выполнить до:")]
+        public DateTime DeadlineTime { get; set; }
+        [Display(Name = "Название")]
+        public string Title { get; set; }
+        [Display(Name = "Текст")]
+        public string Body { get; set; }
+        public IdentityUser User { get; set; }
+        public string UserId { get; set; }
+    }
+}
